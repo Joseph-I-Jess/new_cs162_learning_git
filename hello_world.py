76063e65636c649eef7a21d41e4df0a4e67ad40f
Joseph I Jess <jessj@linnbenton.edu>
2019-10-23 08:48:05 -0700

    Created hello_world.py.
    Added some code to get and print user input.

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+"""Get input from user, print it back to the string."""
+
+
+def main():
+    """Run my tests."""
+    print("Hello?")
+
+    number = input("Please enter a number: ")
+    print(f"You entered {number}")
+
+
+if __name__ == "__main__":
+    main()
